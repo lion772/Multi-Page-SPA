d28da6407f50b56133d21c9786529023dbf30aed
william <william.steinkedemello@gmail.com>
2022-11-21 15:07:23 +0100

    CommentsList and NewCommentForm logic added

--- src/components/comments/CommentsList/CommentsList.tsx
+++ src/components/comments/CommentsList/CommentsList.tsx
@@ -1,12 +1,19 @@
-import React, { FC } from 'react';
-import styles from './CommentsList.module.css';
+import React, { FC } from "react";
+import CommentItem from "../CommentItem/CommentItem";
+import styles from "./CommentsList.module.css";
 
-interface CommentsListProps {}
+interface CommentsListProps {
+    comments: [{ id: string; text: string }];
+}
 
-const CommentsList: FC<CommentsListProps> = () => (
-  <div className={styles.CommentsList}>
-    CommentsList Component
-  </div>
-);
+const CommentsList: FC<CommentsListProps> = (props) => {
+    return (
+        <ul className={styles.comments}>
+            {props.comments.map((comment) => (
+                <CommentItem key={comment.id} text={comment.text} />
+            ))}
+        </ul>
+    );
+};
 
 export default CommentsList;
--- src/components/comments/NewCommentForm/NewCommentForm.tsx
+++ src/components/comments/NewCommentForm/NewCommentForm.tsx
@@ -1,12 +1,30 @@
-import React, { FC } from 'react';
-import styles from './NewCommentForm.module.css';
+import React, { FC, useRef } from "react";
+import styles from "./NewCommentForm.module.css";
 
 interface NewCommentFormProps {}
 
-const NewCommentForm: FC<NewCommentFormProps> = () => (
-  <div className={styles.NewCommentForm}>
-    NewCommentForm Component
-  </div>
-);
+const NewCommentForm: FC<NewCommentFormProps> = () => {
+    const commentTextRef = useRef<HTMLTextAreaElement>(null);
+
+    const submitFormHandler = (event: React.SyntheticEvent) => {
+        event.preventDefault();
+
+        // optional: Could validate here
+
+        // send comment to server
+    };
+
+    return (
+        <form className={styles.form} onSubmit={submitFormHandler}>
+            <div className={styles.control} onSubmit={submitFormHandler}>
+                <label htmlFor="comment">Your Comment</label>
+                <textarea id="comment" rows={5} ref={commentTextRef}></textarea>
+            </div>
+            <div className={styles.actions}>
+                <button className="btn">Add Comment</button>
+            </div>
+        </form>
+    );
+};
 
 export default NewCommentForm;
