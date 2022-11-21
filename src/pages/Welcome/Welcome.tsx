c51c4a9e1d9b4de3d2fe529a646dbd42f3341aeb
william <william.steinkedemello@gmail.com>
2022-11-21 11:34:02 +0100

    Nested route in Welcome component added

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,10 +1,18 @@
 import React, { FC } from "react";
+import { Route } from "react-router-dom";
 import styles from "./Welcome.module.css";
 
 interface WelcomeProps {}
 
-const Welcome: FC<WelcomeProps> = () => (
-    <h1 className={styles.Welcome}>Welcome Page</h1>
-);
+const Welcome: FC<WelcomeProps> = () => {
+    return (
+        <section>
+            <h1 className={styles.Welcome}>Welcome Page</h1>
+            <Route path={"/welcome/new-user"}>
+                <p>Welcome, new user!</p>
+            </Route>
+        </section>
+    );
+};
 
 export default Welcome;
